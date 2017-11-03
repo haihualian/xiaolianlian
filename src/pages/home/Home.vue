47692fbdf5627decf69607ae82c70639797d9555
xiaolianlian <1530993059@qq.com>
2017-11-03 22:22:16 +0800

    ''

---
+++ src/pages/home/Home.vue
@@ -0,0 +1,25 @@
+<template>
+	<div>
+ 		<home-header/>
+ 		<swiper-content/>
+ 		<Acitivity-view/>
+ 	</div>
+</template>
+
+<script>
+
+import HeaderComponent from "./Header";
+import SwiperComponent from "./Swiper";
+import ActivityComponent from "./activity";
+
+export default {
+	components: {
+		"home-header": HeaderComponent,
+		"swiper-content": SwiperComponent,
+		"Acitivity-view": ActivityComponent
+	}
+}
+</script>
+
+<style>
+</style>
